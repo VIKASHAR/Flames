87bf6dd2b52a5f08ffba3e7db405ead5dd69c0d0
Vikash <vikashramesh2021@gmail.com>
2025-02-02 19:02:19 +0530

    vercel commit

--- flames-backend/server.js
+++ flames-backend/server.js
@@ -7,7 +7,14 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST", "GET"],
+        Credentials: true
+    }
+    
+));
 app.use(bodyParser.json());
 
 // MongoDB Connection
