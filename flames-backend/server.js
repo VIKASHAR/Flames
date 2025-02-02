2417eb121c40472e55ff0509492261fbbdf24d00
Vikash <119405655+VIKASHAR@users.noreply.github.com>
2025-02-02 19:38:50 +0530

    Update server.js

--- flames-backend/server.js
+++ flames-backend/server.js
@@ -8,7 +8,7 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors({
-    origin: ["https://flamesvv.vercel.app/"],
+    origin: ["https://flamesvv.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }));
